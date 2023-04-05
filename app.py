54490875cb908c8b62cdcd5b758655ec41ec93a7
ObedRav <obedrayo2356@gmail.com>
2023-04-05 21:13:37 +0000

    Adding rendering to the page

--- app.py
+++ app.py
@@ -3,8 +3,25 @@ from flask import Flask, render_template
 
 app = Flask(__name__)
 
+jobs = [
+    {
+        'title': 'Software Engineer',
+        'location': 'Medellin, Colombia',
+        'salary': '120000'
+    },
+    {
+        'title': 'Database Engineer',
+        'location': 'Bogota, Colombia',
+        'salary': '110000'
+    },
+    {
+        'title': 'Database Engineer',
+        'location': 'Bogota, Colombia'
+    }
+]
+
 @app.route('/')
 def home():
-    return render_template('home.html')
+    return render_template('home.html', jobs=jobs)
 
 
