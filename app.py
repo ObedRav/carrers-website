108c9faf56086d31a572eca6e49c36378e3d96ef
ObedRav <obedrayo2356@gmail.com>
2023-04-07 21:36:09 +0000

    Updating the requirements

--- app.py
+++ app.py
@@ -1,29 +1,16 @@
 #!/usr/bin/python3
 from flask import Flask, render_template, jsonify
+from database import get_jobs
 
 app = Flask(__name__)
 
-jobs = [
-    {
-        'title': 'Software Engineer',
-        'location': 'Medellin, Colombia',
-        'salary': '120000'
-    },
-    {
-        'title': 'Database Engineer',
-        'location': 'Bogota, Colombia',
-        'salary': '110000'
-    },
-    {
-        'title': 'Database Engineer',
-        'location': 'Bogota, Colombia'
-    }
-]
 
 @app.route('/')
 def home():
-    return render_template('home.html', jobs=jobs)
+    JOBS = get_jobs()
+    return render_template('home.html', jobs=JOBS)
 
 @app.route('/api')
 def json():
-    return jsonify(jobs=jobs)
+    JOBS = get_jobs()
+    return jsonify(jobs=JOBS)
--- database.py
+++ database.py
@@ -9,12 +9,34 @@ HOST= getenv('HOST')
 PASSWORD = getenv('PASSWORD')
 DATABASE= getenv('DATABASE')
 
-engine = create_engine(f'mysql+pymysql://{USERNAME}:{PASSWORD}@{HOST}/{DATABASE}?charset=utf8mb4', connect_args={
-    "ssl": {
-        "ssl_ca": "/etc/ssl/cert.pem",
-    }
-}) 
 
-with engine.connect() as conn:
-    result = conn.execute(text('SELECT * FROM jobs;'))
-    print(result.all())
+def connect() -> create_engine.__class__:
+    engine = create_engine(f'mysql+pymysql://{USERNAME}:{PASSWORD}@{HOST}/{DATABASE}?charset=utf8mb4', connect_args={
+        "ssl": {
+            "ssl_ca": "/etc/ssl/cert.pem",
+        }
+    })
+    return engine
+
+def get_jobs() -> dict:
+    engine = connect()
+
+    with engine.connect() as conn:
+        result = conn.execute(text('SELECT * FROM jobs;'))
+
+        list_results = []
+
+        for row in result.all():
+            list_row = list(row)
+            dictionary = {
+                'id': list_row[0],
+                'title': list_row[1],
+                'location': list_row[2],
+                'salary': list_row[3],
+                'currency': list_row[4],
+                'responsibilities': list_row[5],
+                'requirements': list_row[6]
+            }
+            list_results.append(dictionary)
+
+        return(list_results)
