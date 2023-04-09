f4d878833a87a1c40f6965bb9a954a46cd3c0d30
ObedRav <obedrayo2356@gmail.com>
2023-04-09 16:50:21 +0000

    Adding a application form and page

--- app.py
+++ app.py
@@ -1,23 +1,14 @@
 #!/usr/bin/python3
-from flask import Flask, render_template, jsonify
+from flask import Flask, render_template, request
 from database import get_jobs, load_job_from_db
 
 app = Flask(__name__)
 
-JOBS = []
-
-@app.before_request
-def initialize_jobs():
-    global JOBS
-    JOBS = get_jobs()
 
 @app.route('/')
 def home():
+    JOBS = get_jobs()
     return render_template('home.html', jobs=JOBS)
-
-@app.route('/api')
-def json():
-    return jsonify(jobs=JOBS)
 
 @app.route('/job/<int:id>')
 def dinamic_job(id: int):
@@ -26,3 +17,8 @@ def dinamic_job(id: int):
         return "Not found 404"
     return render_template('jobpage.html', job=job) 
 
+@app.route('/job/<int:id>/apply', methods=['POST'])
+def apply_to_job(id: int):
+    data = request.form
+    job = load_job_from_db(id)
+    return render_template('application_submitted.html', application=data, job=job)
